421f0e27c7dc52feb476aaf731fe738bea0a8640
scrappy-strategy-4846 <tomlai8882000@hotmail.com>
2020-06-08 22:55:01 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,9 +2,12 @@ def find_max_value(array)
   count = 0
   
   while array[count] < array.length do
-    
+    if max_value < array[count]
+       max_value = array[count]
+    end 
+  
     
     count += 1 
   end 
-  
+  max_value
 end
